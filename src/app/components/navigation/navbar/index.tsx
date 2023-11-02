f2e8817758c862591ad9cf18c817d3e73cfeef6a
escheive <scheiveerik@gmail.com>
2023-11-02 15:12:31 -0700

    added functionality to navbar and sidebar so for mobile there is a nvbar

--- src/app/components/navigation/navbar/index.tsx
+++ src/app/components/navigation/navbar/index.tsx
@@ -1,6 +1,10 @@
 import Link from 'next/link';
 
-const Navbar = () => {
+const Navbar = ({
+    toggle,
+}: {
+    toggle: () => void;
+}): JSX.Element => {
 
     return (
         <>
@@ -18,6 +22,16 @@ const Navbar = () => {
                             </Link>
                         </li>
                     </ul>
+                    <button type="button" className="inline-flex items-center md:hidden" onClick={toggle}
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg"
+                            width="40"
+                            height="40"
+                            viewBox="0 0 24 24">
+                            <path fill="#fff"
+                                d="M3 6h18v2H3V6m0 5h18v2H3v-2m0 5h18v2H3v-2Z"/>
+                        </svg>
+                    </button>
                 </div>
             </div>
         </>
