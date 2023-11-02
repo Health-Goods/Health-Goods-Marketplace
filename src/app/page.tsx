df7f32da46ecadabf41278f1622da0f141bdbf02
escheive <scheiveerik@gmail.com>
2023-11-02 15:22:37 -0700

    restructured home, and navbar isnow fully responsvie fro mobile with sidebar component working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 
-export default function Home() {
+const Home = () => {
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <h1>Health Goods Marketplace</h1>
-      </div>
+        <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+          <h1>Health Goods Marketplace</h1>
+        </div>
     </main>
   )
 }
+
+export default Home;
