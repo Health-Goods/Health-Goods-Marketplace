a02e39985b0088e5488af0728b2007c657df9730
escheive <scheiveerik@gmail.com>
2023-11-02 14:43:02 -0700

    created a components directory, and an account page

---
+++ src/app/account/page.tsx
@@ -0,0 +1,11 @@
+
+const Account = () => {
+
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-between p-24">
+            <h1>Account Page</h1>
+        </div>
+    )
+}
+
+export default Account;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
+import Navbar from './navbar';
+import Footer from './footer';
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -16,7 +19,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Navbar />
       <body className={inter.className}>{children}</body>
+      <Footer />
     </html>
   )
 }
